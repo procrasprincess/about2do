b09ae01069fa1fa35895d57f2c96103586eb9929
Sarah <xiaowei.lin@outlook.com>
2022-06-17 14:17:23 +0000

    create and import svelte component

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,16 @@
+<div>
+    <form action="" method="">
+        <input type="hidden" name="done" value=""/>
+        <!-- create a button for checkbox -->
+        <button aria-label="Mark Done/NotDone">Done/NotDone</button>
+    </form>
+
+    <form action="" method="">
+        <input type="text"/>
+        <button aria-label="Save To-Do">Save</button>
+    </form>
+
+    <form action="" method="">
+        <button aria-label="Delete To-Do">Delete</button>
+    </form>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- import todo-item svelte component -->
+<script>
+    import TodoItem from "../lib/todo-item.svelte";
+</script>
+
+
+<h1>About To Do</h1>
+<!-- <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
+
+<form action="" method="">
+    <input type="text" name="text" aria-label="Add a To-Do" placeholder="+ type to add a to-do"/>
+</form>
+
+<!-- render todo-item -->
+<TodoItem />
+<TodoItem />
+<TodoItem />
